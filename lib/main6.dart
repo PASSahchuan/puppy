518e02bd2f4ac7c77a018aa415a4b9f844d68324
Eria <c121680118@gmail.com>
2020-07-18 16:12:05 +0800

    SingleChildScrollView

--- lib/main6.dart
+++ lib/main6.dart
@@ -44,134 +44,136 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
         backgroundColor: Colors.deepOrange[300],
       ),
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        margin: EdgeInsets.all(25),
-        alignment: Alignment.center,
-        child: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            Container(
-              child: Image.asset('assets/mainDog.jpg'),
-            ),
-            Container(
-              child: Table(
-                columnWidths: const <int, TableColumnWidth>{
-                  //指定索引及固定列宽
-                  0: FixedColumnWidth(35.0),
-                  1: FixedColumnWidth(100.0),
-                  2: FixedColumnWidth(60.0),
-                  3: FixedColumnWidth(110.0),
-                  4: FixedColumnWidth(35.0),
-                },
-                defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-                children: [
-                  TableRow(
-                    children: [
-                      Container(),
-                      Text(
-                        '城市',
-                        style:
-                            TextStyle(fontSize: 23, color: Color(0xff52616a)),
-                        textAlign: TextAlign.start,
-                      ),
-                      Container(),
-                      DropdownTown(),
-                      Container(),
-                    ],
-                  ),
-                  TableRow(
-                    children: [
-                      Container(),
-                      Text(
-                        '鄉鎮市區',
-                        style:
-                            TextStyle(fontSize: 23, color: Color(0xff52616a)),
-                        textAlign: TextAlign.start,
-                      ),
-                      Container(),
-                      DropdownDistrict(),
-                      Container(),
-                    ],
-                  ),
-                  TableRow(
-                    children: [
-                      Container(),
-                      Text(
-                        '村里',
-                        style:
-                            TextStyle(fontSize: 23, color: Color(0xff52616a)),
-                        textAlign: TextAlign.start,
-                      ),
-                      Container(),
-                      DropdownVillage(),
-                      Container(),
-                    ],
-                  ),
-                  TableRow(
-                    children: [
-                      Container(),
-                      Text(
-                        '調查天數',
-                        style:
-                            TextStyle(fontSize: 23, color: Color(0xff52616a)),
-                        textAlign: TextAlign.start,
-                      ),
-                      Container(),
-                      DropdownOfDay(),
-                      Container(),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            Divider(
-              color: Colors.blueGrey,
-              indent: 20,
-              endIndent: 20,
-            ),
-            Container(
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          margin: EdgeInsets.all(25),
+          alignment: Alignment.center,
+          child: Center(
               child: Column(
-                children: <Widget>[
-                  Text(
-                    '照片中有幾隻狗',
-                    style: TextStyle(fontSize: 23, color: Color(0xff52616a)),
-                    textAlign: TextAlign.center,
-                  ),
-                  DropdownOfNumDog(),
-                ],
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Container(
+                child: Image.asset('assets/mainDog.jpg'),
               ),
-            ),
-            Container(
-              child: Column(
-                children: <Widget>[
-                  Text(
-                    '照片中的狗有幾隻與之前重複',
-                    style: TextStyle(fontSize: 23, color: Color(0xff52616a)),
-                    textAlign: TextAlign.center,
-                  ),
-                  DropdownOfNumDog(),
-                ],
+              Container(
+                child: Table(
+                  columnWidths: const <int, TableColumnWidth>{
+                    //指定索引及固定列宽
+                    0: FixedColumnWidth(35.0),
+                    1: FixedColumnWidth(100.0),
+                    2: FixedColumnWidth(60.0),
+                    3: FixedColumnWidth(110.0),
+                    4: FixedColumnWidth(35.0),
+                  },
+                  defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+                  children: [
+                    TableRow(
+                      children: [
+                        Container(),
+                        Text(
+                          '城市',
+                          style:
+                              TextStyle(fontSize: 23, color: Color(0xff52616a)),
+                          textAlign: TextAlign.start,
+                        ),
+                        Container(),
+                        DropdownTown(),
+                        Container(),
+                      ],
+                    ),
+                    TableRow(
+                      children: [
+                        Container(),
+                        Text(
+                          '鄉鎮市區',
+                          style:
+                              TextStyle(fontSize: 23, color: Color(0xff52616a)),
+                          textAlign: TextAlign.start,
+                        ),
+                        Container(),
+                        DropdownDistrict(),
+                        Container(),
+                      ],
+                    ),
+                    TableRow(
+                      children: [
+                        Container(),
+                        Text(
+                          '村里',
+                          style:
+                              TextStyle(fontSize: 23, color: Color(0xff52616a)),
+                          textAlign: TextAlign.start,
+                        ),
+                        Container(),
+                        DropdownVillage(),
+                        Container(),
+                      ],
+                    ),
+                    TableRow(
+                      children: [
+                        Container(),
+                        Text(
+                          '調查天數',
+                          style:
+                              TextStyle(fontSize: 23, color: Color(0xff52616a)),
+                          textAlign: TextAlign.start,
+                        ),
+                        Container(),
+                        DropdownOfDay(),
+                        Container(),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Container(
-              child: RaisedButton(
-                child: Text('送出'),
-                onPressed: questionnaireSendOut,
-                color: Color(0xfff18904),
+              Divider(
+                color: Colors.blueGrey,
+                indent: 20,
+                endIndent: 20,
               ),
-            ),
-            // Container(
-            //   child: RaisedButton(
-            //     child: Text('登入'),
-            //     onPressed: login,
-            //     color: Color(0xfff18904),
-            //   ),
-            // ),
-          ],
-        )),
+              Container(
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      '照片中有幾隻狗',
+                      style: TextStyle(fontSize: 23, color: Color(0xff52616a)),
+                      textAlign: TextAlign.center,
+                    ),
+                    DropdownOfNumDog(),
+                  ],
+                ),
+              ),
+              Container(
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      '照片中的狗有幾隻與之前重複',
+                      style: TextStyle(fontSize: 23, color: Color(0xff52616a)),
+                      textAlign: TextAlign.center,
+                    ),
+                    DropdownOfNumDog(),
+                  ],
+                ),
+              ),
+              Container(
+                child: RaisedButton(
+                  child: Text('送出'),
+                  onPressed: questionnaireSendOut,
+                  color: Color(0xfff18904),
+                ),
+              ),
+              // Container(
+              //   child: RaisedButton(
+              //     child: Text('登入'),
+              //     onPressed: login,
+              //     color: Color(0xfff18904),
+              //   ),
+              // ),
+            ],
+          )),
+        ),
       ),
     );
   }
