a424ee77f2cc639e85f16b60bebeda6092672986
Eria <c121680118@gmail.com>
2020-07-21 15:44:07 +0800

    創DATA表

--- lib/database/create_db.dart
+++ lib/database/create_db.dart
@@ -1,1 +1,56 @@
+import 'dart:async';
 
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class db_get {
+  Future<Database> create_db() async {
+    final Future<Database> database = openDatabase(
+      // Set the path to the database.
+      join(await getDatabasesPath(), 'doggie_database.db'),
+      // When the database is first created, create a table to store dogs.
+      onCreate: (db, version) {
+        // Run the CREATE TABLE statement on the database.
+        return db.execute(
+          """
+     CREATE TABLE imagup
+  (
+       id INT, 
+       plan TEXT, 
+       user TEXT,
+       imag TEXT,
+       lat REAL,
+       lon REAL,
+       city TEXT,
+       district, TEXT,
+       vilage TEXT,
+       date TEXT,
+       dayCount TEXT,
+       dogCount TEXT,
+       repeatCount TEXT,
+       primary key (id, plan,user)
+    );
+    CREATE TABLE USERE 
+	(
+       plan TEXT, 
+       user TEXT,
+       date TEXT,
+       primary key ( plan,user)
+    );
+    CREATE TABLE timingLocation 
+	(
+       plan TEXT, 
+       user TEXT,
+       time TEXT,
+       lat REAL,
+       lon REAL,
+       primary key (plan,user,time)
+    );""",
+        );
+      },
+      // Set the version. This executes the onCreate function and provides a
+      // path to perform database upgrades and downgrades.
+    );
+    return database;
+  }
+}
