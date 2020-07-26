e4c41f04be1d1ffbe4d7dc4641e38b964731d014
Eria <c121680118@gmail.com>
2020-07-26 11:17:01 +0800

    g

--- lib/page/page1.dart
+++ lib/page/page1.dart
@@ -1,8 +1,11 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 
 class Page1 extends StatefulWidget {
-  Page1({Key key}) : super(key: key);
-
+  Page1({Key key, this.bytes}) : super(key: key);
+  var bytes;
   @override
   _Page1State createState() => _Page1State();
 }
@@ -12,13 +15,22 @@ class _Page1State extends State<Page1> {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.blue,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
+      child: Stack(
         children: <Widget>[
-          Image.asset('assets/mainDog.jpg'),
-          SizedBox(height: 25,),
-          Text('ID'),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.memory(base64Decode(widget.bytes)),
+            ],
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("data"),
+            ],
+          ),
         ],
       ),
     );
