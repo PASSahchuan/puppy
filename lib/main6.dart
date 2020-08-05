269b5bb4affe040bcfa6557cad067d7f7414883a
Eria <c121680118@gmail.com>
2020-08-06 03:09:00 +0800

    原來不是前端的問題e04

--- lib/main6.dart
+++ lib/main6.dart
@@ -402,7 +402,8 @@ class _MyHomePage2State extends State<MyHomePage2> {
         'SELECT MAX(id) FROM imagup WHERE user = ${user[0]["user"]} AND plan = ${user[0]["plan"]}');
     print('當前id===================${imageData[0]['MAX(id)']}=使用者 ${user}=====');
     try {
-      if (imageData[0]['MAX(id)'] == null) {
+      if (imageData[0]['MAX(id)'] == null ||
+          user[0]['id'] > imageData[0]['MAX(id)']) {
         id = user[0]['id'] + 1;
       } else {
         id = imageData[0]['MAX(id)'] + 1;
